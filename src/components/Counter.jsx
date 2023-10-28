e11159f19681c1e0f7aecfbbed639fb897197064
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-10-28 15:26:01 -0300

    Rendering lists at 50:57 on https://www.youtube.com/watch?v=Ke90Tje7VS0

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Counter() {
-	const [count, setCount] = useState(1);
+
+	let [count, setCount] = useState(0);
+	let [classes, setClasses] = useState("mr-2 badge badge-warning");	
+
+	useEffect(() => {
+    if (count === 0) {
+      setClasses("mr-2 badge badge-warning");
+    } else {
+      setClasses("mr-2 badge badge-primary");
+    }
+  }, [count]);
+
+	function addCount() {
+		setCount(count + 1)
+	}
+
+	function subCount() {
+		count > 0 ? setCount(count - 1) : setCount(0)		
+	}
+
 	return (
 		<div className="text-center">
-			<span className="badge badge-primary mr-2">
+			<span className={classes}>
 				{count === 0 ? 'ZERO' : count}
 				</span>
-			<button className="btn btn-sm btn-secondary mr-2">Decrement -</button>
-			<button className="btn btn-sm btn-success">Increment +</button>
+			<button 
+				className="btn btn-sm btn-secondary mr-2" 
+				onClick={subCount}>Decrement -</button>
+			<button 
+				className="btn btn-sm btn-success" 
+				onClick={addCount}>Increment +</button>
 		</div>
 	)
 }
